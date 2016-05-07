e11c5d32a398c937f02ae21c723529e57bec8714
Mark Durrant <mark.j.durrant@gmail.com>
2016-05-07 20:19:16 +0100

    add strip shape

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,71 @@
-var svgString = "176.2,24.9 76.8,15.3 111.4,119.6 91.2,138.4 115.9,194.3 100.8,248.8 308.7,360.7 298.1,434 315.1,440.7 376.4,343.2 345.5,240.5 379.7,209.9 342.1,180.9 339.2,132.3 244,93.4 214.1,114 ";
+var svgString = "176.2,24.9 76.8,15.3 111.4,119.6 91.2,138.4 115.9,194.3 100.8,248.8 308.7,360.7 298.1,434 315.1,440.7 376.4,343.2 345.5,240.5 379.7,209.9 342.1,180.9 339.2,132.3 244,93.4 214.1,114";
 
 svgString = svgString.split(" ");
 
-var svgPoints = [];
-var paperPoints = [];
+var shapePoints = [];
 for (var s = 0; s < svgString.length; s++) {
-  svgPoints.push(svgString[s].split(","));
+  var a = svgString[s].split(",");
+
+  var x = Number(a[0]);
+  var y = Number(a[1]);
+
+  shapePoints.push(new Point(x, y));
 }
 
-function arrayToPoint(array) {
-  var x = array[0];
-  var y = array[1];
+var flat = new Path(shapePoints);
+    flat.closed = true;
+    flat.strokeColor = 'blue';
 
-  var point = new Point(Number(x), Number(y));
-  return point;
+var shapeDistances = []
+for (var i = 0; i < shapePoints.length; i++) {
+  var d = 0;
+
+  if (i != shapePoints.length -1) {
+    d = shapePoints[i].getDistance(shapePoints[i + 1]);
+  } else {
+    d = shapePoints[i].getDistance(shapePoints[0]);
+  }
+
+  shapeDistances.push(d);
 }
 
-for (var p = 0; p < svgPoints.length; p++) {
-  paperPoints.push(arrayToPoint(svgPoints[p]));
+var startPoint = new Point(100, flat.bounds.height + 100);
+var myHeight = 200;
+
+var tabWidth = 10;
+var tabHieght = 10;
+var tabPos = .5;
+
+var myX = startPoint.x;
+for (var i = 0; i < shapePoints.length; i++) {
+  myX = myX + shapeDistances[i];
+  var path = new Path();
+      path.style = {strokeColor: 'green', dashArray: [4, 4]};
+      path.add(new Point(myX, startPoint.y));
+      path.add(new Point(myX, startPoint.y + myHeight));
+
+  if(shapeDistances[i] > tabWidth * 4) {
+      var path2 = new Path();
+        path2.style = {strokeColor: 'green'};
+        path2.add(new Point(myX - shapeDistances[i] / 2 + tabWidth / 2, startPoint.y + myHeight * tabPos));
+        path2.add(new Point(myX - shapeDistances[i] / 2 + tabWidth / 2, startPoint.y + myHeight * tabPos - tabHieght));
+
+    var path3 = new Path();
+        path3.style = {strokeColor: 'green'};
+        path3.add(new Point(myX - shapeDistances[i] / 2 - tabWidth / 2, startPoint.y + myHeight * tabPos));
+        path3.add(new Point(myX - shapeDistances[i] / 2 - tabWidth / 2, startPoint.y + myHeight * tabPos - tabHieght));
+
+    var path4 = new Path();
+        path4.style = {strokeColor: 'green'};
+        path4.add(new Point(myX - shapeDistances[i] / 2 - tabWidth / 2, startPoint.y + myHeight * tabPos));
+        path4.add(new Point(myX - shapeDistances[i] / 2 + tabWidth / 2, startPoint.y + myHeight * tabPos));
+  }
 }
 
-var path = new Path(paperPoints);
-    path.closed = true;
-    path.strokeColor = 'black';
-
-console.log(paperPoints);
+new Path(
+  startPoint,
+  startPoint + [myX, 0],
+  startPoint + [myX, myHeight],
+  startPoint + [0, myHeight],
+  startPoint
+).style = {strokeColor: 'green'};

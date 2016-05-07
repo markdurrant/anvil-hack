837ceead2d88a8fe7720f71a99668cdbd7b6008c
Mark Durrant <mark.j.durrant@gmail.com>
2016-05-07 18:10:17 +0100

    add design & hack svg to paper import

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,27 @@
-// Create a Paper.js Path to draw a line into it:
-  var path = new Path();
-  // Give the stroke a color
-  path.strokeColor = 'black';
-  var start = new Point(100, 100);
-  // Move to start and draw a line from there
-  path.moveTo(start);
-  // Note the plus operator on Point objects.
-  // PaperScript does that for us, and much more!
-  path.lineTo(start + [ 100, -50 ]);
+var svgString = "176.2,24.9 76.8,15.3 111.4,119.6 91.2,138.4 115.9,194.3 100.8,248.8 308.7,360.7 298.1,434 315.1,440.7 376.4,343.2 345.5,240.5 379.7,209.9 342.1,180.9 339.2,132.3 244,93.4 214.1,114 ";
+
+svgString = svgString.split(" ");
+
+var svgPoints = [];
+var paperPoints = [];
+for (var s = 0; s < svgString.length; s++) {
+  svgPoints.push(svgString[s].split(","));
+}
+
+function arrayToPoint(array) {
+  var x = array[0];
+  var y = array[1];
+
+  var point = new Point(Number(x), Number(y));
+  return point;
+}
+
+for (var p = 0; p < svgPoints.length; p++) {
+  paperPoints.push(arrayToPoint(svgPoints[p]));
+}
+
+var path = new Path(paperPoints);
+    path.closed = true;
+    path.strokeColor = 'black';
+
+console.log(paperPoints);

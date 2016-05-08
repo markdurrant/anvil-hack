9d988b0aa17f0fa78af8a79b3e053c706cf63b38
Mark Durrant <mark.j.durrant@gmail.com>
2016-05-08 13:45:25 +0100

    all the things

---
+++ conts-data.js
@@ -0,0 +1,114 @@
+var cd = [
+  {
+    name: "barnet and camden",
+    file: "barnet-camden.svg",
+    svgData: "461.7,308.6 347.2,355.6 477.9,494.5 549,529.8 578.4,506 515.8,432.8 485.5,436.2 513.4,351.3",
+    total: 183124,
+    lab: 74927,
+    tory: 73722
+  },
+  {
+    name: "bexley and bromley",
+    file: "bexley-bromley.svg",
+    svgData: "491.6,449 583.5,570.5 596.4,647.7 665.7,633.3 655.3,608.6 701.2,552.1 726.5,333.1 683,282.5 648.4,290.9 667,336.1 617.4,424.6",
+    total: 188624,
+    lab: 48134,
+    tory: 97433
+  },
+  {
+    name: "brent and harrow",
+    file: "brent-harrow.svg",
+    svgData: "390.9,338.2 301.7,393.3 340.5,481.2 371.5,461.7 466.4,499.6 522,476.9",
+    total: 173191,
+    lab: 75143,
+    tory: 67042
+  },
+  {
+    name: "city and east",
+    file: "city-east.svg",
+    svgData: "376.4,555.2 455.8,495 529.1,499.7 564.4,418.3 623.6,466.5 596,539.5 444.7,580.5",
+    total: 209266,
+    lab: 125908,
+    tory: 40194
+  },
+  {
+    name: "croydon and sutton",
+    file: "croydon-sutton.svg",
+    svgData: "349,361.5 375.4,444.8 404.7,424.6 441.7,460 448.4,488.1 484.2,499.9 593.5,411.5 502.9,290.4 442.5,303 457.9,344.5",
+    total: 180694,
+    lab: 63964,
+    tory: 76378
+  },
+  {
+    name: "ealing and hillingdon",
+    file: "ealing-hillingdon.svg",
+    svgData: "551,371.3 649.7,381.3 688.5,469.2 719.5,449.7 814.4,487.6 816.3,536 668.1,556.8 673.7,593.9 626.9,628.3 576.1,604.3 590.6,546.6 565.5,491.4 587,473",
+    total: 198515,
+    lab: 81955,
+    tory: 76337
+  },
+  {
+    name: "enfield and haringey",
+    file: "enfield-haringey.svg",
+    svgData: "511.3,442.3 647,435.7 658.3,496.6 616,610.7 525.8,624.1 553.3,539.4 499.7,495.6",
+    total: 166265,
+    lab: 87946,
+    tory: 45069
+  },
+  {
+    name: "greenwich and lewisham",
+    file: "greenwich-lewisham.svg",
+    svgData: "",
+    total: 162134,
+    lab: 84507,
+    tory: 39141
+  },
+  {
+    name: "havering and redbridge",
+    file: "havering-redbridge.svg",
+    svgData: "345.5,470.8 352.4,584 427.1,587.7 465.6,508 519.7,554.9 494,627.5 537.5,678.1 655.3,558.7 551.1,445.5 475.5,447.1 393.3,505.6",
+    total: 169839,
+    lab: 59711,
+    tory: 75299
+  },
+  {
+    name: "lambeth and southwark",
+    file: "lambeth-southwark.svg",
+    svgData: "520.4,335.7 592.7,361.5 553.9,487.4 493.5,500 471.6,431.8 482.1,398.1 491.6,361.7",
+    total: 186851,
+    lab: 103829,
+    tory: 38216
+  },
+  {
+    name: "merton and wandsworth",
+    file: "merton-wandsworth.svg",
+    svgData: "416.7,367 436.3,380.6 457,345.8 495.7,335.5 484.6,371.8 521.9,481.5 415,499.7 383.3,397.1",
+    total: 184835,
+    lab: 80291,
+    tory: 72952
+  },
+  {
+    name: "north east",
+    file: "north-east.svg",
+    svgData: "581.3,401.1 615.5,426.5 624.3,539.4 541.4,598 478.8,524.8 538.9,516.8",
+    total: 227674,
+    lab: 136848,
+    tory: 40341
+  },
+  {
+    name: "south west",
+    file: "south-west.svg",
+    svgData: "582,632.1 569.3,705.7 589.8,712.3 649,615.7 617.3,513.1 650.7,483 614.3,452 466.1,472.8 471.7,509.9 424.9,544.3",
+    total: 212733,
+    lab: 70224,
+    tory: 97141
+  },
+  {
+    name: "west central",
+    file: "west-central.svg",
+    svgData: "462.6,525.2 515,500.2 590.2,532.7 579.1,573.1 541,580.8 522.2,616.2 464.3,571",
+    total: 153216,
+    lab: 55329,
+    tory: 70490
+  }
+];
